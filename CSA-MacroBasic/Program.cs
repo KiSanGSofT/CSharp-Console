8c43f4ff7944a423867a146cb4514c45c782f1f1
kisangsoft <kisangsoft@naver.com>
2023-07-28 02:05:31 +0900

    매크로 기본 추가

---
+++ CSA-MacroBasic/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CSA_MacroBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowWidth = 100;
+            Console.WindowHeight = 30;
+
+            Console.WriteLine("\n----- 시작 / Press Any Key -----\n");
+            Console.ReadKey();
+
+            //----------
+
+            Thread.Sleep(5000);
+
+            Thread t = new Thread(() =>
+            {
+                while (true)
+                {
+                    SendKeys.SendWait("매크로 문자열 입력{Enter}");
+                    Thread.Sleep(1000);
+                    /*
+                    if (Keyboard.IsKeyDown(Key.Right))
+                    {
+                        SendKeys.SendWait(" \n 매크로 문자열 입력");
+                        // SendKeys.Send("^c"); // 단축키
+                        // SendKeys.Send("{Enter}"); // 특수키
+                    }
+                    */
+                }
+            });
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+
+            //----------
+
+            Console.WriteLine("\n----- 종료 / Press Any Key -----\n");
+
+            Console.ReadKey();
+        }
+    }
+}
