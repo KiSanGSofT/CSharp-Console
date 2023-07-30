2fd248e60227e4a84708a9a3b1411dfd34a30184
kisangsoft <kisangsoft@naver.com>
2023-07-31 00:39:23 +0900

    최초커밋 - Do While 반복문

---
+++ CSA-Do-While-Loop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSA_Do_While_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowWidth = 100;
+            Console.WindowHeight = 30;
+
+            Console.WriteLine("\n----- 시작 / Press Any Key -----\n");
+            Console.ReadKey();
+
+            //----------
+
+            int varInt = 1;
+
+            do
+            {
+                Console.WriteLine(varInt);
+
+                varInt++;
+
+            } while (varInt <= 10);
+
+            //----------
+
+            Console.WriteLine("\n----- 종료 / Press Any Key -----\n");
+
+            Console.ReadKey();
+        }
+    }
+}
