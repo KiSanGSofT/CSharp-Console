5c8317e9ad64b7e00f084610d2b2b06a4557c03b
kisangsoft <kisangsoft@naver.com>
2023-08-01 02:18:31 +0900

    C# @ String 축간 문자열 추가

---
+++ CSA-Vertatim-String/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSA_Vertatim_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowWidth = 100;
+            Console.WindowHeight = 30;
+
+            Console.WriteLine("\n----- 시작 / Press Any Key -----\n");
+            Console.ReadKey();
+
+            //----------
+
+            string vertatimText = @"있는 그대로
+                        문자열 그대로
+                        작성한 그대로
+                        화면에 출력한다.
+                        그래서 멀티라인도 가능하다.";
+
+            Console.WriteLine(vertatimText);
+
+            Console.WriteLine();
+
+            vertatimText = vertatimText + @"
+                다른 문자열을 추가할 때는
+                @ 문자를 사용하고
+                변수 또는 문자열을 덧붙이면 된다.
+            ";
+
+            Console.WriteLine(vertatimText);
+
+            Console.ReadKey();
+
+            //----------
+
+            Console.WriteLine("\n----- 종료 / Press Any Key -----\n");
+
+            Console.ReadKey();
+        }
+    }
+}
