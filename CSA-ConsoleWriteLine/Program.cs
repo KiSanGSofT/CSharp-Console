912cdc86869b0775d5160cada514ab41e7045d27
DevB <cruiseradar@gmail.com>
2023-08-15 22:50:42 +0900

    C# 문자열 화면 출력

---
+++ CSA-ConsoleWriteLine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSA_ConsoleWriteLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n----- 시작 / 문자열 호출 -----\n");
+            Console.ReadKey();
+
+            Console.WriteLine("안녕 Hello あんねい");
+
+            Console.WriteLine("안녕");
+            Console.WriteLine("Hello");
+            Console.WriteLine("あんねい");
+
+            //----------
+
+            Console.WriteLine("\n----- 종료 / Press Any Key -----\n");
+            Console.ReadKey();
+        }
+    }
+}
