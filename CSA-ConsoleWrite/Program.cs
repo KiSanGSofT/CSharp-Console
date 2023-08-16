c3d86c831019fe9aa7b96c904921bd80568b9d9e
DevB <cruiseradar@gmail.com>
2023-08-17 01:35:24 +0900

    C# 콘솔 화면 출력

---
+++ CSA-ConsoleWrite/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSA_ConsoleWrite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n----- 시작 / 문자열 호출 -----\n");
+            Console.ReadKey();
+
+            //----------
+
+            Console.Write("안녕 Hello あんねい");
+
+            Console.Write("안녕");
+            Console.Write("Hello");
+            Console.Write("あんねい");
+
+            //----------
+
+            Console.WriteLine("\n----- 종료 / Press Any Key -----\n");
+            Console.ReadKey();
+        }
+    }
+}
